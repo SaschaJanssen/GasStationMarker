6512353869dcf710761db1d29a95f18e00983555
Dennis Dierkes <dierkes@smartsquare.de>
2018-11-16 16:32:47 +0100

    Add radius to GasStationFinder

--- src/main/kotlin/de/smartsquare/gas/gasstation/GasStationFinder.kt
+++ src/main/kotlin/de/smartsquare/gas/gasstation/GasStationFinder.kt
@@ -10,20 +10,19 @@ class GasStationFinder {
 
     fun findAllStationsInRadius(start: LatLng, radius: Double): Maybe<List<GasStations.GasStation>> =
         "https://creativecommons.tankerkoenig.de/json/list.php"
-            .httpGet(parametersOf(start))
+            .httpGet(parametersOf(start, radius))
             .rx_responseObject(GasStations.Deserializer())
             .toMaybe()
             .filter { it -> it.second is Result.Success }
             .map { it -> it.second.component1()?.stations }
 
-
-    private fun parametersOf(it: LatLng): List<Pair<String, String>> {
+    private fun parametersOf(start: LatLng, radius: Double): List<Pair<String, String>> {
         return listOf(
-                "lat" to "${it.lat}",
-                "lng" to "${it.lng}",
-                "rad" to "1",
-                "sort" to "dist",
-                "type" to "all",
+            "lat" to "${start.lat}",
+            "lng" to "${start.lng}",
+            "rad" to "${radius}",
+            "sort" to "dist",
+            "type" to "all",
             "apikey" to "CHANGEME"
         )
     }
