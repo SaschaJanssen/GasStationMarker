57573e30ec036642f0df090dad7538d354729018
Dennis Dierkes <dierkes@smartsquare.de>
2018-11-16 16:30:02 +0100

    Reduce GasStationFinder to one request

--- src/main/kotlin/de/smartsquare/gas/gasstation/GasStationFinder.kt
+++ src/main/kotlin/de/smartsquare/gas/gasstation/GasStationFinder.kt
@@ -1,30 +1,21 @@
 package de.smartsquare.gas.gasstation
 
-import com.github.kittinunf.fuel.core.FuelError
-import com.github.kittinunf.fuel.core.Response
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.rx.rx_responseObject
 import com.github.kittinunf.result.Result
 import com.google.maps.model.LatLng
 import io.reactivex.Maybe
-import io.reactivex.Observable
 
 class GasStationFinder {
 
-    fun findCheapestGasStation(markers: List<LatLng>): Maybe<GasStations.GasStation> {
-        val requests: List<Observable<Pair<Response, Result<GasStations, FuelError>>>> = markers
-            .map { "https://creativecommons.tankerkoenig.de/json/list.php" to parametersOf(it) }
-            .map { it -> it.first.httpGet(it.second) }
-            .map { it -> it.rx_responseObject(GasStations.Deserializer()).toObservable() }
-
-        return Observable.merge(requests)
+    fun findAllStationsInRadius(start: LatLng, radius: Double): Maybe<List<GasStations.GasStation>> =
+        "https://creativecommons.tankerkoenig.de/json/list.php"
+            .httpGet(parametersOf(start))
+            .rx_responseObject(GasStations.Deserializer())
+            .toMaybe()
             .filter { it -> it.second is Result.Success }
             .map { it -> it.second.component1()?.stations }
-            .flatMapIterable { it -> it }
-            .distinct()
-            .sorted(compareBy(GasStations.GasStation::diesel))
-            .firstElement()
-    }
+
 
     private fun parametersOf(it: LatLng): List<Pair<String, String>> {
         return listOf(
